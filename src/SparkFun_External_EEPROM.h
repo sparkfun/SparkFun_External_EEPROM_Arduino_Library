9be30a9072dbcfdb2f4c3816963c7fb82e120827
TinaJ_kay <44362472+TylerBird@users.noreply.github.com>
2021-01-06 10:55:46 +0100

    Add support for STM32 for I2C buffer size
    In the Wire.h for STM32 a `#define BUFFER_SIZE 32` is available.

--- src/SparkFun_External_EEPROM.h
+++ src/SparkFun_External_EEPROM.h
@@ -54,6 +54,12 @@
 #define I2C_BUFFER_LENGTH_RX I2C_BUFFER_LENGTH
 #define I2C_BUFFER_LENGTH_TX I2C_BUFFER_LENGTH
 
+#elif defined(STM32)
+#elif defined(STM32)
+
+#define I2C_BUFFER_LENGTH_RX BUFFER_LENGTH //BUFFER_LENGTH is defined in Wire.h for STM32
+#define I2C_BUFFER_LENGTH_TX BUFFER_LENGTH
+
 #else
 
 #pragma GCC error "This platform doesn't have a wire buffer size defined. Please contribute to this library!"
